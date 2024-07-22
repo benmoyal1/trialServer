368264a1e8a37f28ec857a1dc7ab7c7fe7f019bc
benmoyal1 <benmoyal2013@gmail.com>
2024-07-22 22:11:33 +0300

    chaged port to 433:

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { Pool } = require("pg");
 
 const app = express();
-const port = 3000;
+const port = 80;
 
 // PostgreSQL connection configuration
 const pool = new Pool({
